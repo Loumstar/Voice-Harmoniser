ec9ba50ffbe0a3524b619ab8d53ea7dcc24d4616
Loumstar <louis.manestar@gmail.com>
2019-05-07 08:51:14 +0100

    Bug fixing

--- c/pitch_detection/fourier_transform.h
+++ c/pitch_detection/fourier_transform.h
@@ -1,17 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <complex.h>
-#include <stdlib.h>
 
-#define PI 3.14159265358979
+#define PI M_PI
  
-void _fft(double complex waveform[], double complex spectrum[], int n, int step)
+void _fft(double complex waveform[], double complex spectrum[], size_t n, int step)
 {
 	if(step < n){
 		_fft(spectrum, waveform, n, step * 2);
 		_fft(spectrum + step, waveform + step, n, step * 2);
 		
-        for (int a = 0; a < n; a += 2 * step){
+        for (size_t a = 0; a < n; a += 2 * step){
 			double complex t = cexp(-I * PI * a / n) * spectrum[a + step];
 			
             waveform[a / 2] = spectrum[a] + t;
@@ -20,19 +20,18 @@ void _fft(double complex waveform[], double complex spectrum[], int n, int step)
 	}
 }
  
-double complex* signalCopy(double complex waveform[], int n){
+double complex* signalCopy(double complex waveform[], size_t n){
     double complex* copy = malloc(sizeof(double complex) * n);
-    for(int a = 0; a < n; a++){
+    for(size_t a = 0; a < n; a++){
         copy[a] = waveform[a];
     }
     return copy;
 }
 
-double complex fft(double complex waveform[])
+void fft(double complex waveform[], size_t length)
 {
-	int n = sizeof(*waveform)/sizeof(double complex);
-	double complex* spectrum = signalCopy(waveform, n);
-	_fft(waveform, spectrum, n, 1);
-    
-    return *waveform;
+	double complex* spectrum = signalCopy(waveform, length);
+	_fft(waveform, spectrum, length, 1);
+    free(spectrum);
+    printf("FFT\n");
 }
--- c/pitch_detection/pitch_detector.c
+++ c/pitch_detection/pitch_detector.c
@@ -1,111 +1,158 @@
-#include <math.h>
 #include <stdbool.h>
 #include "./fourier_transform.h"
 
-#define FRAME_RATE 44100
-#define BIT_DEPTH pow(16, 2)
+#define FRAME_RATE 4096
+#define BIT_DEPTH 1
 
-#define CLIP_LENGTH 0.1
+#define CLIP_LENGTH 1
 
-#define CLIP_FRAMES FRAME_RATE * CLIP_LENGTH
+#define CLIP_FRAMES (size_t) round(FRAME_RATE * CLIP_LENGTH)
 
 #define THRESHOLD 3
-#define PROBABILITY_THRESHOLD 0.01
 
 #define DISTRIBUTION_SPACING 50
 
-#define HARMONICS_ARR_SIZE 20
-#define SAMPLE_ARR_SIZE 75
-#define PEAKS_ARR_SIZE 20
+#define HARMONICS_ARR_SIZE (size_t) 20
+#define SAMPLE_ARR_SIZE (size_t) 75
+#define PEAKS_ARR_SIZE (size_t) 20
 
-int* get_spectrum(){
-    int* s = malloc(sizeof(int) * CLIP_FRAMES);
-    for(int i = 0; i < CLIP_FRAMES / 2; i++){
-        s[i] = i / CLIP_LENGTH;
+#define EULER M_E
+
+void print_complex_array(double complex complex_arr[], size_t s){
+    for(size_t j = 0; j < s; j++){
+        printf("%f + i%f\n", creal(complex_arr[j]), cimag(complex_arr[j]));
     }
-    return s;
 }
 
-double mean(double complex arr[]){
-    int sum = 0,
-        length = sizeof(*arr) / sizeof(double complex);
-    for(int i = 0; i < length; i++){
-        sum += cimag(arr[i]);
+void print_magnitude(double complex complex_arr[], size_t s){
+    for(size_t j = 0; j < s; j++){
+        printf("%lu Hz: %f\n", j, cabs(complex_arr[j]));
     }
-    return sum / length;
+}
+
+int* get_spectrum(){
+    int* spectrum = malloc(sizeof(int) * CLIP_FRAMES);
+    for(size_t i = 0; i < floor(CLIP_FRAMES / 2); i++){
+        spectrum[i] = i / CLIP_LENGTH;
+    }
+    return spectrum;
+}
+
+double mean(double complex arr[], size_t length){
+    double sum = 0;
+    for(size_t i = 0; i < length; i++){
+        sum += arr[i];
+    }
+    printf("Offset: %f\n", sum);
+    return (double) sum / length;
 }
 
 void remove_offset(double complex clip[], double offset){
-    int length = sizeof(*clip) / sizeof(double complex);
-    for(int i = 0; i < length; i++){
-        clip[i] = creal(clip[i]) + (I * ((cimag(clip[i]) / BIT_DEPTH) - offset));
+    for(size_t i = 0; i < CLIP_FRAMES; i++){
+        clip[i] = (clip[i] - offset) / BIT_DEPTH;
     }
 }
 
 void _decompose(double complex clip[]){
-    double offset = mean(clip);
+    double offset = mean(clip, CLIP_FRAMES);
     remove_offset(clip, offset);
-    fft(clip);
+    fft(clip, CLIP_FRAMES);
+    print_complex_array(clip, 10);
+    print_magnitude(clip, 100);
 }
 
 double get_noise_level(int f, double complex clip[]){
     double sum = 0;
-    for(int i = f - floor(SAMPLE_ARR_SIZE); i < f + floor(SAMPLE_ARR_SIZE); i++){
+    size_t lb, ub;
+    if(f + floor(SAMPLE_ARR_SIZE / 2) > CLIP_FRAMES){
+        lb = CLIP_FRAMES - SAMPLE_ARR_SIZE;
+        ub = CLIP_FRAMES;
+    } else if(f - floor(SAMPLE_ARR_SIZE / 2) < 0){
+        lb = 0;
+        ub = SAMPLE_ARR_SIZE;
+    } else {
+        lb = f - floor(SAMPLE_ARR_SIZE / 2);
+        ub = f + floor(SAMPLE_ARR_SIZE / 2);
+    }
+    for(size_t i = lb; i < ub; i++){
         sum += cimag(clip[i]);
     }
-    return sum / SAMPLE_ARR_SIZE;
+    return (double) sum / SAMPLE_ARR_SIZE;
 }
 
-bool _is_maxima(double complex y0, double complex y1, double complex y2){
-    if((cimag(y1) - cimag(y0) > 0) && (cimag(y2) - cimag(y1) < 0)){
-        return true;
-    } else {
-        return false;
+bool _is_maxima(double y0, double y1, double y2){
+    return (y1 - y0 > 0) && (y2 - y1 < 0);
+}
+
+double* get_harmonics(double peak){
+    double* harmonics = malloc(sizeof(double) * HARMONICS_ARR_SIZE);
+    for(int h = 0; h < HARMONICS_ARR_SIZE; h++){
+        harmonics[h] = peak * (h + 1);
     }
+    return harmonics;
 }
 
-double complex* get_peaks(double complex clip[]){
-    int length = sizeof(*clip) / sizeof(double complex);
-    int peaks_count = 0;
+double get_correlation(double f, double complex peaks[]){
+    double c = 0;
+    for(size_t s = 0; s < PEAKS_ARR_SIZE; s++){
+        c += pow(EULER, (2 * (f - creal(peaks[s]) / DISTRIBUTION_SPACING)));
+    }
+    return c;
+}
+
+double test_harmonics(double complex peaks[], double harmonics[]){
+    double correlation = 0;
+    for(int h = 0; h < HARMONICS_ARR_SIZE; h++){
+        correlation += get_correlation(harmonics[h], peaks);
+    }
+    return correlation / HARMONICS_ARR_SIZE;
+}
+
+void get_peaks(double complex clip[]){
     double noise;
-    for(int f = 0; f < length; f++){
+    for(size_t f = 0; f < CLIP_FRAMES; f++){
         noise = get_noise_level(f, clip);
-        if(cimag(clip[f]) / noise > THRESHOLD && _is_maxima(clip[f-1], clip[f], clip[f+1])){
-            peaks_count++;
+        if(cimag(clip[f]) / noise > THRESHOLD && 
+        _is_maxima(cimag(clip[f-1]), cimag(clip[f]), cimag(clip[f+1]))){
+            clip[f] = creal(clip[f]) + (I * creal(clip[f]) / noise);
         } else {
-            clip[f] = (double complex) 0;
+            clip[f] = 0;
         }
     }
-    double complex* peaks = malloc(sizeof(double complex) * peaks_count);
-    int f = 0;
-    int p = 0;
-    while(p < peaks_count){
-        if(clip[f] != (double complex) 0){
-            peaks[p] = clip[f];
-            p++;
-        }
-        f++;
-    }
-    return peaks;
 }
 
-double complex* get_note_probabilities(double complex peaks[]){
-    int length = sizeof(*peaks) / sizeof(double complex);
-    
-    double complex* notes = malloc(sizeof(double complex) * length);
-    int* harmonics = malloc(sizeof(int) * HARMONICS_ARR_SIZE);
-    
+int sort_peaks(const void* a, const void* b){
+    return cimag(*(double complex*)b) - cimag(*(double complex*)a);
+}
+
+void get_note_probabilities(double complex peaks[]){    
     double probability;
-    for(int p = 0; p < length; p++){
-        harmonics = get_harmonics(peaks[p]);
+    for(int p = 0; p < PEAKS_ARR_SIZE; p++){
+        double* harmonics = get_harmonics(creal(peaks[p]));
         probability = test_harmonics(peaks, harmonics);
-        notes[p] = creal(peaks[p]) + (probability * I);
+
+        free(harmonics);
+
+        peaks[p] = creal(peaks[p]) + (I * probability);
     }
-    return notes;
 }
 
-double complex* get_pitch(double complex clip[]){
+void get_pitch(double complex clip[]){
     _decompose(clip);
-    double complex* peaks = get_peaks(clip);
-    return get_note_probabilities(peaks);
+
+    //printf("Decomposed:\n");
+    //print_complex_array(clip, CLIP_FRAMES);
+
+    
+    //get_peaks(clip);
+    //qsort(clip, CLIP_FRAMES, sizeof(double complex), sort_peaks);
+    //from this point on, use PEAKS_ARR_SIZE as the array is now in descending order
+
+    //printf("Peaks:\n");
+    //print_complex_array(clip, PEAKS_ARR_SIZE);
+
+    //get_note_probabilities(clip);
+    
+    //printf("Notes\n");
+    //print_complex_array(clip, PEAKS_ARR_SIZE);
 }
