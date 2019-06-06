28659916e467527d8a68fbbbffa4fcc0e510c54d
Loumstar <louis.manestar@gmail.com>
2019-06-06 21:59:58 +0100

    Added bounds to note frequencies

--- c/midi_input/create_json_frequencies.py
+++ c/midi_input/create_json_frequencies.py
@@ -1,5 +1,5 @@
 import re, json
-
+"""
 with open('./piano_frequencies.txt') as f:
     frequency_list = f.readlines()
 
@@ -14,3 +14,27 @@ for f in frequency_list:
 
 with open('./piano_frequencies.json', 'w+') as g:
     g.write(json.dumps(json_list, indent=4, sort_keys=True))
+
+"""
+
+with open('./piano_frequencies.json', 'r') as f:
+    frequencies_json = json.loads(f.read())
+
+for i, freq in enumerate(frequencies_json):
+    frequencies_json[i]['number'] = len(frequencies_json) + 3 - i
+    if i != 0 and i != len(frequencies_json) - 1:
+        frequencies_json[i]['bounds'] = {
+            'lb': round(float(freq['frequency']) + (0.5 * (float(frequencies_json[i+1]['frequency']) - float(freq['frequency']))), 6),
+            'ub': round(float(freq['frequency']) + (0.5 * (float(frequencies_json[i-1]['frequency']) - float(freq['frequency']))), 6)
+        }
+    elif i == 0:
+        frequencies_json[i]['bounds'] = {
+                    'lb': round(float(freq['frequency']) + (0.5 * (float(frequencies_json[i+1]['frequency']) - float(freq['frequency']))), 6),
+        }
+    else:
+        frequencies_json[i]['bounds'] = {
+            'ub': round(float(freq['frequency']) + (0.5 * (float(frequencies_json[i-1]['frequency']) - float(freq['frequency']))), 6)
+        }
+
+with open('./piano_frequencies.json', 'w') as g:
+    g.write(json.dumps(frequencies_json, indent=4, sort_keys=True))
