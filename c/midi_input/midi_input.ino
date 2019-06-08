e8566fb7c877d86508bdce54ea79423c03085728
Loumstar <louis.manestar@gmail.com>
2019-06-09 00:03:23 +0100

    Added functions to midi script

--- c/midi_input/midi_input.ino
+++ c/midi_input/midi_input.ino
@@ -1,21 +1,50 @@
 #include <SoftwareSerial.h>
+#include <math.h>
 
 SoftwareSerial midiSerial(10, 11);
+int msg[3];
+
+void note_off(double freq);
+void note_on(double freq, int vol);
+
+double note_frequency(int note){
+    return pow(2, (note - 69) / 12) * 440.0;
+}
+
+void handle_midi(int* msg){
+    if(msg[0] < 0xA0){
+        double f = note_frequency(msg[1]);
+        if(msg[0] < 0x90){
+            note_off(f);
+        } else {
+            note_on(f, msg[2]);
+        }
+    }
+}
+
+int* midi_read(int* msg, size_t msg_length){
+    size_t i = 0;
+    while(i < msg_length){
+        msg[i] = midiSerial.read(); //need to check for status byte
+        i++;
+    }
+    while(i < 3){
+        msg[i] = 0x00;
+        i++;
+    }
+}
 
 void setup(){
-    Serial.begin(9600);
-    while(!Serial);
-
-    midiSerial.begin(31250);
+    Serial.begin(9600); // USB baud rate
+    while(!Serial); // While USB connection has not been established
+    midiSerial.begin(31250); // MIDI baud rate
 }
 
 void loop(){
     midiSerial.listen();
     
-    while(midiSerial.available() > 0){
-        Serial.write(
-            midiSerial.read()
-        );
+    if(midiSerial.available()){
+        midi_read(msg, midiSerial.available());
+        handle_midi(msg);
     }
-
 }
