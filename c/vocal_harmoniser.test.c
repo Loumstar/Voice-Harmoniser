3fd6a902b813ccc9412404427e150089d53e51b4
Loumstar <louis.manestar@gmail.com>
2019-11-08 18:51:13 +0000

    Git commit to add the first complete integration test of the vocal harmoniser.
    Although the reharmonised audio file is failing, it is showing that all components are working together successfully.
    The main problem is to do with transitioning from one sample to another, where some smoothing algorithm will need to be used.

--- c/vocal_harmoniser.test.c
+++ c/vocal_harmoniser.test.c
@@ -1,13 +1,14 @@
 #include <stdint.h>
 #include <complex.h>
+#include <math.h>
 
 #include "pitch_detection.h"
 #include "reharmoniser.h"
 #include "wave_file.h"
 
 int main(void){
-    char sample_filename[] = "elements_vocals_sample.wav";
-    char reharmonised_filename[] = "elements_vocals_reharmonised.wav";
+    char sample_filename[] = "Testing C-note.wav";
+    char reharmonised_filename[] = "note_reharmonised_to_chord.wav";
 
     note notes[MAX_VOICES] = {
         {40, 261.6, 128},
@@ -28,10 +29,14 @@ int main(void){
     // Read wave file to sample
     Wave sample = read_wave(sample_filename);
 
-    sample_buffer_size = (size_t) sample.header.sample_rate * sample_buffer_time;
+    sample_buffer_size = (size_t) pow(2, round(log2f(sample.header.sample_rate * sample_buffer_time)));
+
+    printf("Sample Buffer Length: %.2f ms\n", ((float) sample_buffer_size / sample.header.sample_rate) * 1000);
+
+    size_t sample_size = pow(2, ceil(log2f(sample.numberof_samples)));
 
     // Allocate memory for the sample array
-    sample_array = malloc(sample.numberof_samples * sizeof(int32_t));
+    sample_array = malloc(sample_size * sizeof(int32_t));
     // Write the data to the sample array
     read_wave_data_to_array(&sample, sample_array);
 
@@ -40,21 +45,21 @@ int main(void){
     // Allocate memory for the circular buffer with complex type (for pitch detection)
     complex_buffer = malloc(sample_buffer_size * sizeof(double complex));
 
-    for(size_t i = 0; i < sample.numberof_samples; i += sample_buffer_size){
-        for(size_t j = 0; j < sample_buffer_size && (i + j) < sample.numberof_samples; j++){
+    for(size_t i = 0; i < sample_size; i += sample_buffer_size){
+        for(size_t j = 0; j < sample_buffer_size && (i + j) < sample_size; j++){
             buffer[j] = sample_array[i + j];
-            complex_buffer[j] = (double) (sample_array[i + j] * PD_SAMPLE_BIT_DEPTH / INT32_MAX);
+            complex_buffer[j] = (double) sample_array[i + j];
         }
 
-        pitch = get_pitch(complex_buffer);            
-        
-        for(size_t j = 0; j < sample_buffer_size && (i + j) < sample.numberof_samples; j++){
+        pitch = get_pitch(complex_buffer, sample_buffer_size, sample.header.sample_rate, sample.header.bits_per_sample);            
+
+        for(size_t j = 0; j < sample_buffer_size && (i + j) < sample_size; j++){
             sample_array[i + j] = get_reharmonised_wave_amplitude(buffer, notes, pitch, j, sample_buffer_size);
         }
     }
 
-    write_array_to_wav_file(reharmonised_filename, sample_array, sample.numberof_samples, 1, sample.header.sample_rate, sample.header.bits_per_sample);
-
+    write_array_to_wav_file(reharmonised_filename, sample_array, sample_size, 1, sample.header.sample_rate, sample.header.bits_per_sample);
+    
     free(complex_buffer);
     free(sample_array);
     free(buffer);
